12004e4ed55e81a7e3865777eb80b0c2b86b09b7
One Better <17625608203@163.com>
2018-09-26 21:53:19 +0800

    修改第三次

--- app.py
+++ app.py
@@ -1,8 +1,39 @@
 #!/usr/bin/env python3
 # -*- coding=utf-8 -*-
+import tornado.web
+from tornado.options import options, define
+import tornado.options
+import tornado.ioloop
+
+from handlers import main
+
+define('port', default=8000, help="listening port", type=int)
 
 
+class App(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            ('/', main.IndexHandler),
+            ('/explore', main.ExploreHandler),
+            ('/post/(?P<post_id>[0-9]+)', main.PostHandler)
 
+        ]
+
+        settings = dict(
+            debug=True,
+            template_path='templates'
+        )
+
+        super(App, self).__init__(handlers, **settings)
+
+
+application = App()
+if __name__ == "__main__":
+    print('Starting Server......')
+    tornado.options.parse_command_line()
+    application.listen(options.port)
+    print('Server  started')
+    tornado.ioloop.IOLoop.current().start()
 
 
 
--- handlers/main.py
+++ handlers/main.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding=utf-8 -*-
 
+import tornado.web
 
 
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self, *args, **kwargs):
+        self.render('index.html')
 
 
+class ExploreHandler(tornado.web.RequestHandler):
+    def get(self, *args, **kwargs):
+        self.render('explore.html')
+
+
+class PostHandler(tornado.web.RequestHandler):
+    def get(self, post_id):
+        self.render('post.html', post_id=post_id)
 
